ba86be7b6be7709bb979c2680363b52329065624
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-10-03 09:39:42 +0530

    Add files via upload

---
+++ OLYMPICS24.py
@@ -0,0 +1,18 @@
+# cook your dish here
+a,b,c=map(int,input().split())
+s=0
+if a==5:
+    s=s+0
+else:
+    s=5-a
+if b==5:
+    s=s+0
+else:
+    s+=(5-b)
+if c==5:
+    s=s+0
+else:
+    s+=(5-c)
+print(s)
+
+
