7f7a18b18ceeee8cf4e5d0d8f0006f925f9fd048
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-12-03 09:00:20 +0530

    Add files via upload

---
+++ CREDITS.py
@@ -0,0 +1,11 @@
+# cook your dish here
+x=int(input())
+for _ in range(x):
+    a=int(input())
+    if a>65:
+        print('Overload')
+    elif a<35:
+        print('Underload')
+    else:
+        print("Normal")
+    
