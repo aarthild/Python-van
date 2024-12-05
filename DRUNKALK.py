34d9ce13027bd295863f45a1a2ab44366cc81790
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-12-05 20:27:26 +0530

    Add files via upload

---
+++ DRUNKALK.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for t in range(int(input())):
+    c = 0
+    for i in range(int(input())):
+        if i%2 == 0:
+            c+=3
+        else:
+            c-=1
+    print(c)
+
