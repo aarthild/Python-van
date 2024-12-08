82edacad0089e09e6b637b5efa5e0f607fd7d05e
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-12-08 14:12:51 +0530

    Add files via upload

---
+++ DNASTRAND.py
@@ -0,0 +1,11 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    n=int(input())
+    s=input()
+    d={'A':'T', 'T':'A', 'C':'G','G':'C'}
+    l=''
+    for i in range(len(s)):
+        if s[i] in d:
+            l+=d[s[i]]
+    print(l)
