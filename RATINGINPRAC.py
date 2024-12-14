41028c063ad066cc5cc48f1a6fe9a1e0da6dfdfc
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-12-14 19:48:34 +0530

    Add files via upload

---
+++ RATINGINPRAC.py
@@ -0,0 +1,11 @@
+t = int(input())
+def Difficulty_rating(d):
+    for i in range(1,len(d)):
+        if (d[i] < d[i-1]):    
+            return "No"
+    return "Yes"
+for _ in range(t):
+    n = int(input())
+    d = list(map(int, input().split()))
+    print(Difficulty_rating(d))
+    
