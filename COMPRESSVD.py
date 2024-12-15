3fe0bc6443089df4be5c3bc671e0e36e6faf7dfb
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-12-15 19:33:48 +0530

    Add files via upload

---
+++ COMPRESSVD.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    a=list(map(int,input().split()))
+    m=1
+    for j in range(0,n-1):
+        if a[j]!=a[j+1]:
+            m+=1
+    print(m)
