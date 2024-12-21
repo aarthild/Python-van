687ef43d0fb2f65ab87cf8ae1237be0ac2154307
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-12-21 19:33:54 +0530

    Add files via upload

---
+++ gfg2.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+#User function Template for python3
+
+class Solution:
+    def get_min_max(self, arr):
+        a=min(arr)
+        b=max(arr)
+        return a,b
+    
