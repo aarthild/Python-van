0bb08ccaafe12bdef2f1bfe7ccb06d62a52dba27
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2024-12-25 20:17:50 +0530

    Add files via upload

---
+++ CHANGEPOS.py
@@ -0,0 +1,8 @@
+# cook your dish here
+# cook your dish here
+for i in range(int(input())):
+    a,b,c,d=map(int,input().split())
+    if a==c or b==d:
+        print("2")
+    else:
+        print("1")
