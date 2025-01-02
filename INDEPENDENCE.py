71cdb67ab62f72aa17b3f1a16d008a97df19b9fa
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-01-02 19:35:51 +0530

    Add files via upload

--- INDEPENDENCE.py
+++ INDEPENDENCE.py
@@ -1,16 +1,7 @@
 # cook your dish here
-t=int(input())
-for I in range (t) :
-    
-    a, b, c=map(int, input().split())
-    if (a==b and b==c):
-        print("Yes")
-     
-    elif(a>=b and a>=c and (b+c+1)>=a) :
-        print ("Yes")
-    elif(b>=a and b>=c and (c+a+1)>=b):
-        print("Yes")
-    elif(c>=a and c>=b and (a+b+1)>=c):
-        print("Yes")
+for i in range(int(input())):
+    a,b,c=map(int,input().split())
+    if 2*max(a,b,c)<=a+b+c+1:
+        print('yes')
     else:
-        print("No")
+        print('no')
