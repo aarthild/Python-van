133c47e4cd483cf36bdc3155ddfd6e5e6379aeb9
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-01-08 20:49:43 +0530

    Add files via upload

---
+++ INSTDUM.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for i in range (int(input())):
+    n=int(input())
+    l= list(map(int,input().split()))
+    s=0
+    count=0
+    for i in range(0,n):
+        s = s + l[i]
+        if s==i+1:
+            count=count+1
+    print(count)
