c94e5b55fb76f84a748e7469067cc1409be03c01
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-01-18 17:20:50 +0530

    Add files via upload

---
+++ POPCORN.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    c,d=map(int,input().split())
+    e,f=map(int,input().split())
+    print(max(a+b,c+d,e+f))
+    
