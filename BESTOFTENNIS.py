19770a45254ce7c7f371bd60a214bc464bf46433
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-01-26 11:11:19 +0530

    Add files via upload

--- BESTOFTENNIS.py
+++ BESTOFTENNIS.py
@@ -1,5 +1,10 @@
 # cook your dish here
-for _ in range(int(input())):
-    X,Y = map(int,input().split())
-    a = max(X,Y)
-    print((2*a)-1)
+a=int(input())
+for i in range(a):
+    a,b=map(int,input().split())
+    c=max(a,b)
+    d=min(a,b)
+    e=c-d-1
+    print((a+b)+e)
+    
+    
