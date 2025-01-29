6922c285cce875f6dd044d83175cd1c4871d3828
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-01-29 18:53:35 +0530

    Add files via upload

---
+++ DARLIG.py
@@ -0,0 +1,15 @@
+# cook your dish here
+
+for _ in range(int(input())):
+
+    n, k = map(int, input().split())
+    if (k == 0):
+        if (n % 4 != 0):
+            print("ON")
+        else:
+            print("OFF")
+    else:
+        if (n % 4 != 0):
+            print("AMBIGUOUS")
+        else:
+            print("ON")
