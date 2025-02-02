929d111eaf31b63288ed6873fffd6496706caf29
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-02-02 19:51:56 +0530

    Add files via upload

--- PENALTYSHOOT.py
+++ PENALTYSHOOT.py
@@ -1,8 +1,20 @@
 # cook your dish here
-t = int(input())
-for _ in range(t):
-    a, b = map(int, input().split())
-    if a + 2 >= b and b + 1 >= a:
-        print("YES")
+
+
+
+for _ in range(int(input())):
+
+    x, y = map(int, input().split())
+    if x==y:
+        print("yes")
+    elif x>y:
+        if y+1<x:
+            print("no")
+        else:
+            print('yes')
     else:
-        print("NO")
+        if x+2<y:
+            print('no')
+        else:
+            print('yes')
+    
