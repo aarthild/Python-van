15602d87a75abd40930ae06c10538ba6b9372991
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-02-02 19:51:43 +0530

    Add files via upload

--- GROUPASSGN.py
+++ GROUPASSGN.py
@@ -1,5 +1,5 @@
-# cook your dish here
-for i in range(int(input())):
-    n,x=map(int,input().split())
-    a=1+(2*n)
-    print(a-x)
+for _ in range(int(input())):
+
+    n, x = map(int, input().split())
+
+    print(2*n - x + 1)
