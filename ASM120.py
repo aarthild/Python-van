7eb1f84d951390ebd47fc144107d3fea19fcaa05
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-02-10 17:23:22 +0530

    Add files via upload

---
+++ ASM120.py
@@ -0,0 +1,12 @@
+# cook your dish here
+def gcd(a,b):
+
+    if(b==0):
+
+        return a
+    else:
+        return gcd(b,a%b)
+t=int(input())
+for _ in range(t):
+    a,b=map(int,input().split())
+    print(gcd(a,b))
