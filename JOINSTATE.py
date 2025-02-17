baaa1c20610bf05a1f2f5c49e68baef035c676da
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-02-17 19:18:50 +0530

    Add files via upload

---
+++ JOINSTATE.py
@@ -0,0 +1,19 @@
+# cook your dish here
+
+for i in range(int(input())):
+
+    n,m=map(int,input().split())
+    a=list(map(int,input().split()))
+    
+    s=0
+    ans=0
+    for i in a:
+        if i<m:
+            s+=i
+            if s>=m:
+                ans+=1
+                s=0
+        else:
+            ans+=1
+            s=0
+    print(ans)
