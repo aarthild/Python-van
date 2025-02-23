c2e88b675581e3641c1017f391b2686240d8ea14
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-02-23 12:41:11 +0530

    Add files via upload

---
+++ HEADBOB.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for i in range(int(input())):
+    a=int(input())
+    b=input()
+    if "I" in b:
+        print("INDIAN")
+    elif "Y" in b:
+        print("NOT INDIAN")
+    else:
+        print("NOT SURE")
